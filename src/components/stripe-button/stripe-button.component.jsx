4e02223f1d27653d530ecb4d9f4428c793316417
igor <igor.atf@gmail.com>
2020-05-27 18:31:06 -0300

    change collection component to use context and set valid api keys

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_l4aqe0SsS6G3f3sU9LLb7FeF00tdeLsfBr';
 
   const onToken = token => {
     console.log(token);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+    apiKey: "AIzaSyDXCYcIKQtbtmOYGUTattaOmP17oZ8yBZU",
+    authDomain: "ecommerce-crwn-db.firebaseapp.com",
+    databaseURL: "https://ecommerce-crwn-db.firebaseio.com",
+    projectId: "ecommerce-crwn-db",
+    storageBucket: "ecommerce-crwn-db.appspot.com",
+    messagingSenderId: "771237642203",
+    appId: "1:771237642203:web:5170694e852b159314b1fe",
+    measurementId: "G-07V6ML38QY"
 };
 
 firebase.initializeApp(config);
